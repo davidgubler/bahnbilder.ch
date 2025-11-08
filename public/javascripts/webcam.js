f95221bb6f157aafd2328a81f8acb1dea8ef6b8d
David Gubler <david@kabelverhau.ch>
2025-11-08 19:40:20 +0100

    use current video ID

--- public/javascripts/webcam.js
+++ public/javascripts/webcam.js
@@ -24,7 +24,7 @@ var onYouTubePlayerAPIReady = function() {
 	player = new YT.Player('ytplayer', {
 		width: '100%',
 		height: 'auto',
-		videoId: 'Y1MlZfLpiec',
+		videoId: 'FvnVvXyj16A',
 		allow: "autoplay",
 		events: {
 			'onStateChange': onPlayerStateChange,
