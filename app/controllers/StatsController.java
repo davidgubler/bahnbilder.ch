388504d687ba06b4ad109680b089710f59586c6c
David Gubler <david@kabelverhau.ch>
2025-11-25 19:57:26 +0100

    show consolidated request stats

--- app/controllers/StatsController.java
+++ app/controllers/StatsController.java
@@ -11,10 +11,11 @@ import utils.Config;
 import utils.Context;
 import utils.NotAllowedException;
 
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class StatsController extends Controller {
+
     public Result show(Http.Request request) {
         Context context = Context.get(request);
         User user = context.getUsersModel().getFromRequest(request);
@@ -29,6 +30,8 @@ public class StatsController extends Controller {
         }
         System.out.println("stats records for today: " + requestsDaily.getUrlStats().size());
 
+        Set<String> domains = new HashSet<>();
+
         String hostEn = Config.Option.HOST_EN.get();
         String hostDe = Config.Option.HOST_DE.get();
         List<? extends UrlStats> externalReferers = requestsDaily.getUrlStats().values().stream().filter(s -> {
@@ -42,9 +45,36 @@ public class StatsController extends Controller {
             if (hostDe != null && (domain.equals(hostDe) || domain.endsWith("." + hostDe))) {
                 return false;
             }
+            domains.add(domain);
             return true;
-        }).collect(Collectors.toUnmodifiableList());
+        }).toList();
 
-        return ok(views.html.stats.view.render(request, externalReferers, user, lang));
+        Set<String> consolidated = new HashSet<>();
+        Map<String, String> consolidatorLookup = new HashMap<>();
+        domains.stream().sorted(Comparator.comparing(String::length)).forEach(domain -> {
+            int p = domain.lastIndexOf(".");
+            while (p > 0) {
+                String upperDomainPart = domain.substring(p + 1);
+                if (consolidated.contains(upperDomainPart)) {
+                    consolidatorLookup.put(domain, upperDomainPart);
+                    return;
+                }
+                p = domain.substring(0, p).lastIndexOf(".");
+            }
+            consolidated.add(domain);
+            consolidatorLookup.put(domain, domain);
+        });
+
+        Map<String, List<UrlStats>> consolidatedRefererUrlStats = new HashMap<>();
+        externalReferers.forEach(s -> {
+            String domain = s.getReferer().split("/")[0];
+            String consolidatedDomain = consolidatorLookup.get(domain);
+            if (!consolidatedRefererUrlStats.containsKey(consolidatedDomain)) {
+                consolidatedRefererUrlStats.put(consolidatedDomain, new ArrayList<>());
+            }
+            consolidatedRefererUrlStats.get(consolidatedDomain).add(s);
+        });
+
+        return ok(views.html.stats.view.render(request, consolidatedRefererUrlStats, user, lang));
     }
 }
