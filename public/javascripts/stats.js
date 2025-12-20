cd2fdaf8f553daa54f4b02a3c47943f9d4ca223a
David Gubler <david@kabelverhau.ch>
2025-12-20 19:15:56 +0100

    Expand domains on demand

--- public/javascripts/stats.js
+++ public/javascripts/stats.js
@@ -2,4 +2,8 @@ $(document).ready(function() {
 	$("select[name=period]").change(function() {
         window.location.href = "/stats/" + $("select[name=period]").val();
 	});
+
+	$("span.request-domain").click(function(e) {
+	    $(e.target).parent().find("div.request-list").toggle();
+	});
 });
