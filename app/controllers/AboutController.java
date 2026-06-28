4299d64805455f1e969ce367d970b647bdd4109c
David Gubler <david@kabelverhau.ch>
2026-06-28 10:33:16 +0200

    wip

--- app/controllers/AboutController.java
+++ app/controllers/AboutController.java
@@ -8,6 +8,7 @@ import play.mvc.Http;
 import play.mvc.Result;
 import services.MongoDb;
 import utils.Context;
+import utils.NotFoundException;
 
 public class AboutController extends Controller {
     @Inject
@@ -18,6 +19,21 @@ public class AboutController extends Controller {
         User user = context.getUsersModel().getFromRequest(request);
         String lang = Lang.get(request);
         return ok(views.html.about.view.render(request, user, lang));
+    }
+
+    public Result user(Http.Request request, Integer userId) {
+        Context context = Context.get(request);
+        User user = context.getUsersModel().getFromRequest(request);
+        String lang = Lang.get(request);
+
+        User aboutUser = context.getUsersModel().get(userId);
+        if (aboutUser == null) {
+            throw new NotFoundException("user");
+        }
+
+        int userSince = 2012;
+
+        return ok(views.html.about.user.render(request, aboutUser, userSince, user, lang));
     }
 
     public Result privacy(Http.Request request) {
